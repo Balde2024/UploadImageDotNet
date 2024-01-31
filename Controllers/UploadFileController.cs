2c3c3a27c2b9a9b85ab854685569671251065dae
Balde620 <abdourahamane.balde@gmail.com>
2024-01-31 19:53:15 +0100

    first commit

---
+++ Controllers/UploadFileController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUpload.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadFileController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Upload([FromForm ] SingleFileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+
+                //create folder if not exist
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                //get file extension
+                FileInfo fileInfo = new FileInfo(model.File.FileName);
+                string fileName = model.FileName + fileInfo.Extension;
+
+                string fileNameWithPath = Path.Combine(path, fileName);
+
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    model.File.CopyTo(stream);
+                }
+                
+            }
+            return Ok("Succès") ;
+        }
+    }
+}
---
+++ Model/SingleFileModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileUpload.Model
+{
+	public class SingleFileModel
+	{
+        [Required(ErrorMessage = "Please enter file name")]
+        public string FileName { get; set; }
+        [Required(ErrorMessage = "Please select file")]
+        public IFormFile File { get; set; }
+
+        public SingleFileModel()
+		{
+		}
+	}
+}
+
